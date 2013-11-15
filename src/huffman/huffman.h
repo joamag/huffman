98e8adc5352bd9a72e41906632f3244835985dec
joamag <joamag@gmail.com>
2013-11-15 13:16:36 +0000

    new disable of warnings

--- src/huffman/huffman.h
+++ src/huffman/huffman.h
@@ -1,7 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <viriatum_commons/viriatum_commons.h>
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #ifndef PATH_MAX
 #define PATH_MAX MAX_PATH
 #endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <viriatum_commons/viriatum_commons.h>
